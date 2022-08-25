ac18d28824d615394f2706e0871a642b5fe3d725
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-08-25 10:52:33 +0200

    Change guess word

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import Dummy from './Dummy';
 import SolutionLetters from './SolutionLetters';
 import ErrorLetters from './ErrorLetters';
 import Form from './Form';
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from 'react-router-dom';
 import Footer from './Footer';
 import Instructions from './Instructions';
 import Options from './Options';
@@ -43,19 +43,36 @@ function App() {
       setUserLetters([...userLetters]);
     }
   };
+  const changeWord = (value) => {
+    setWord(value);
+    setUserLetters([]);
+    setLastLetter('');
+  };
 
   return (
     <div className="page">
-
       <Header />
       <main className="main">
         <section>
           <Routes>
-            <Route path="/" element={<><SolutionLetters word={word} userLetters={userLetters} />
-          <ErrorLetters word={word} userLetters={userLetters} />
-          <Form lastLetter={lastLetter} handleLastLetter={handleLastLetter} /></>}/>
-          <Route path="/instructions" element={<Instructions />}/>
-          <Route path="/options" element={<Options />}/>
+            <Route
+              path="/"
+              element={
+                <>
+                  <SolutionLetters word={word} userLetters={userLetters} />
+                  <ErrorLetters word={word} userLetters={userLetters} />
+                  <Form
+                    lastLetter={lastLetter}
+                    handleLastLetter={handleLastLetter}
+                  />
+                </>
+              }
+            />
+            <Route path="/instructions" element={<Instructions />} />
+            <Route
+              path="/options"
+              element={<Options word={word} changeWord={changeWord} />}
+            />
           </Routes>
         </section>
         <Dummy numberOfErrors={getNumberOfErrors()} />
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,5 +1,29 @@
-const Options = () => {
-    return <div>Estas son las opciones del juego</div>
-}
+const Options = (props) => {
+  const handleChange = (ev) => {
+    props.changeWord(ev.currentTarget.value);
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label className="title" htmlFor="word">
+        Escribe aquí la palabra que hay que adivinar:
+      </label>
+      <input
+        autoFocus
+        autoComplete="off"
+        className="form__input"
+        maxLength="15"
+        type="text"
+        id="word"
+        name="word"
+        onChange={handleChange}
+        value={props.word}
+      />
+    </form>
+  );
+};
 
 export default Options;
